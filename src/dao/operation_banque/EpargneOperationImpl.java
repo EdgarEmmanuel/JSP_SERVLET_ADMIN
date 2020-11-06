f29625b1899eb1b0d1d7d4d974420fc2748203ad
Edgar <mmnledgar@gmail.com>
2020-11-06 11:41:06 +0000

    pattern_abstract_factory

---
+++ src/dao/operation_banque/EpargneOperationImpl.java
@@ -0,0 +1,23 @@
+package dao.operation_banque;
+
+public class EpargneOperationImpl implements IOperation_Bancaire {
+
+	@Override
+	public String virementBancaireOperation(int compteDestinataire, int montant) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String depotBancaireOperation(int compteDestinataire, int montant) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String RetraitBancaireOperation(int compteDestinataire, int montant) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/dao/operation_banque/IOperation_Bancaire.java
+++ src/dao/operation_banque/IOperation_Bancaire.java
@@ -1,5 +1,7 @@
 package dao.operation_banque;
 
 public interface IOperation_Bancaire {
-	public String executerOperationBancaire(int compteDestinataire,int montant,String nomOperation);
+	public String virementBancaireOperation(int compteDestinataire,int montant);
+	public String depotBancaireOperation(int compteDestinataire,int montant);
+	public String RetraitBancaireOperation(int compteDestinataire,int montant);
 }
